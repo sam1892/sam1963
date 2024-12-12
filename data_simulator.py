8151195bc2fd7a816828bdc1e5e8a834c4b5b52e
sam1892 <90484444+sam1892@users.noreply.github.com>
2024-12-11 20:50:19 -0600

    Update data_simulator.py

--- data_simulator.py
+++ data_simulator.py
@@ -3,19 +3,22 @@ import time
 import datetime
 import requests
 
-url = "http://localhost:5000/ingest-data"
+url = "http://127.0.0.1:5000/ingest-data"
 
 def generate_data():
     return {
-        'timestamp': datetime.datetime.now().isoformat(),
-        'temperature': round(random.uniform(70, 90), 2),
-        'vibration': round(random.uniform(0.001, 0.005), 4),
-        'wind_speed': round(random.uniform(10, 20), 2),
-        'failure': random.choice([0, 1])
+        "timestamp": datetime.datetime.now().isoformat(),
+        "temperature": round(random.uniform(70, 90), 2),
+        "vibration": round(random.uniform(0.001, 0.005), 4),
+        "wind_speed": round(random.uniform(10, 20), 2),
+        "failure": random.choice([0, 1])
     }
 
 while True:
     data = generate_data()
-    response = requests.post(url, json=data)
-    print(f"Data sent to PMSWT: {data}")
+    try:
+        response = requests.post(url, json=data)
+        print(f"Data sent: {data}")
+    except Exception as e:
+        print(f"Error sending data: {e}")
     time.sleep(5)
